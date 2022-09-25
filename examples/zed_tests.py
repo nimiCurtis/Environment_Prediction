a408179127ee7c707f9a7072d669830fa49beb3b
nimiCurtis <nimicu21@gmail.com>
2022-09-25 13:52:27 +0300

    add difference

--- examples/zed_tests.py
+++ examples/zed_tests.py
@@ -4,8 +4,32 @@ import time
 import numpy as np
 import os
 
-if __name__ == "__main__":
+def difference_heatmap(image1,image2):
+    return cv2.subtract(image1, image2)
 
+def cv_heatmap(zed,depth_zed):
+    zed.retrieve_image(depth_zed, sl.VIEW.DEPTH)
+    image_depth_ocv = depth_zed.get_data()
+    heatmap = cv2.applyColorMap(image_depth_ocv[:,:,0],cv2.COLORMAP_JET)
+    return heatmap
+
+def abs_heatmap(zed,depth):
+    zed.retrieve_measure(depth, sl.MEASURE.DEPTH)
+    depth_vals = depth.get_data()
+    np.nan_to_num(depth_vals)
+    depth_inter = (np.interp(depth_vals, [0,3000],[255,0]))
+    depth_inter = np.array(depth_inter, dtype=np.uint8)
+    heatmap = cv2.applyColorMap(depth_inter,cv2.COLORMAP_JET)
+    return heatmap
+    
+
+def confidence_map(zed,confidence):
+    zed.retrieve_measure(confidence, sl.MEASURE.CONFIDENCE)
+    image_depth_ocv = confidence.get_data()
+    return image_depth_ocv
+    
+    
+def main():
     print("Running Depth Sensing sample ... ")
 
     init = sl.InitParameters(camera_resolution=sl.RESOLUTION.HD720,
@@ -32,18 +56,38 @@ if __name__ == "__main__":
     time.sleep(3)
     while k<10:
         if zed.grab(init_runtime) == sl.ERROR_CODE.SUCCESS:
-            zed.retrieve_image(depth_zed, sl.VIEW.DEPTH)
-            image_depth_ocv = depth_zed.get_data()
-
-            #zed.retrieve_measure(confidence, sl.MEASURE.CONFIDENCE)
-            #image_depth_ocv = confidence.get_data()
             
-            heatmap = cv2.applyColorMap(image_depth_ocv[:,:,0],cv2.COLORMAP_JET)
-            name_heat = os.path.join(dir_name,str(k)+"_conf70heat.jpg")
-            cv2.imwrite(name_heat,heatmap)
+            # save heatmap of opencv (relative heatmap)
+            #image_depth_ocv = cv_heatmap(zed,depth_zed)
+            
+            # save confidence map
+            #image_depth_ocv = confidence_map(zed, confidence)
+            
+            # save abs heat map
+            image_depth_ocv = abs_heatmap(zed, depth_zed)
+            
+            # save difference heatmap
+            if k==0: depth_prev = image_depth_ocv.copy()
+            image_difference = difference_heatmap(image_depth_ocv, depth_prev)
+            depth_prev = image_depth_ocv.copy()
+            
+            
+            name = os.path.join(dir_name,str(k)+"_conf70heat.jpg")
+            cv2.imwrite(name,image_depth_ocv)
+            cv2.imwrite(name, image_difference)
             print("Saved image number " + str(k))
-
             k+=1
-            time.sleep(0.5)
-    
+            time.sleep(0.1)
     print("finishing..")
+            
+if __name__ == "__main__":
+             main()
+
+
+            
+            
+            
+
+            
+    
+    
